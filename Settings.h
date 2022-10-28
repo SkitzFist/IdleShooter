2e08e0bb2f58a6ce4461a2b43b77079b11d5f32a
SkitzFist <jo.persson1@hotmail.se>
2022-10-28 15:30:28 +0200

    Updated settings with namespace

--- Settings.h
+++ Settings.h
@@ -4,11 +4,15 @@
 
 #include "Vec2.h"
 
-static constexpr const Vec2<int>  WORLD_SIZE = {800,600};
- static constexpr const int CELL_SIZE = 32;
- static constexpr const Vec2<int> GRID_SIZE = {WORLD_SIZE.x / CELL_SIZE, WORLD_SIZE.y / CELL_SIZE +1};
+namespace Settings
+{
+    static constexpr const Vec2<int>  WORLD_SIZE = {800,600};
+    static constexpr const int CELL_SIZE = 32;
+    static constexpr const Vec2<int> GRID_SIZE = {WORLD_SIZE.x / CELL_SIZE, WORLD_SIZE.y / CELL_SIZE +1};
+    
+    //DEBUG
+    static bool debugMenu = true;    
 
- //DEBUG
- static bool debugMenu = true;
+} // namespace Settings
 
 #endif
