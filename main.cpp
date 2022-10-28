9d706bec2c85637891475d141ac6ed1e0d0960bd
SkitzFist <jo.persson1@hotmail.se>
2022-10-28 15:06:49 +0200

    Reworked flow

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
-#include "include/raylib.h"
 #include "include/emscripten.h"
-#include "Game.h"
+#include "include/raylib.h"
+
 #include <string>
 
+void run();
+void destroy();
 
-int main(void)
-{
-    Game game;
-    game.run();
-    return 0;
+int main(void){
+    emscripten_set_main_loop(run, 60,1);
+    destroy();
 }
 
+void destroy(){
+    std::string str = "Destroy";
+    TraceLog(LOG_INFO, str.c_str());
+}
+
+void run(){
+     std::string str = "LOOP";
+    TraceLog(LOG_INFO, str.c_str());
+}
