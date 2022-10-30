157470e2ce67e91a37ef68366174a088117b314e
SkitzFist <jo.persson1@hotmail.se>
2022-10-30 13:58:06 +0100

    Add Sort

---
+++ Sort.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef _Sort
+#define _Sort
+
+#include "Vec2.h"
+
+namespace Sort{
+    template <typename T>
+    static void swap(T* a, T* b){
+        T tmp = *a;
+        *a = *b;
+        *b =  tmp;
+    };
+
+    template<typename T>
+    static int partition(Array<T>& vec, int low, int high){
+        int pivotIndex = high;
+        int i = (low - 1);
+        for(int j = low; j <= high - 1; ++j){
+            if(vec[j] < vec[pivotIndex]){
+                ++i;
+                swap(&vec[i], &vec[j]);
+            }
+        }
+        swap(&vec[i+1], &vec[high]);
+        return (i+1);
+    };
+
+    template<typename T>
+    static void quickSort(Array<T>& vec, int low, int high){
+        if(low < high){
+            int pi = partition(vec, low, high);
+            quickSort(vec, low, pi - 1);
+            quickSort(vec, pi + 1, high);
+        }
+    };
+}
+
+#endif
