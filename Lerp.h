1629c1114492e56c38a78b5df193ee3ee48a5ac3
SkitzFist <jo.persson1@hotmail.se>
2022-11-15 13:40:51 +0100

    Add Lerp functions

--- Lerp.h
+++ Lerp.h
@@ -4,16 +4,34 @@
 
 namespace Lerp
 {
-    static constexpr const float smoothStart(float t){
-        return t * t;
+    static constexpr const float lerp(float time, float startPos, float target){
+       return (target - startPos) * time + startPos;
     }
 
-    static constexpr const float smoothStart2(float t){
-        return t * t *t;
+    static constexpr const float smoothStop(float time){
+        float x = 1.f - time;
+        return 1.f - (x * x);
     }
 
-    static constexpr const float test(float t){
-        return smoothStart(t) * -1;
+    static constexpr const float smoothStop2(float time){
+        float x = 1.f - time;
+        return 1.f - (x * x * x);
+    }
+
+    static constexpr const float smoothStart(float time){
+        return time * time;
+    }
+
+    static constexpr const float smoothStart2(float time){
+        return time * time * time;
+    }
+
+    static constexpr const float smoothStep(float time){
+        return Lerp::lerp(time, Lerp::smoothStart(time), Lerp::smoothStop(time));
+    }
+
+    static constexpr const float smoothStep2(float time){
+        return Lerp::lerp(time, Lerp::smoothStart2(time), Lerp::smoothStop2(time));
     }
 } // namespace Lerp
 
